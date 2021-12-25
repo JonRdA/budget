c77dba007f19d8bee3f871944cf20dfc8c6c3d24
Jon Ruiz de Azua <ruizdeazua.jon@gmail.com>
2021-12-25 19:26:25 +0100

    Add timeline and breakdown wrapper functions

--- src/report.py
+++ src/report.py
@@ -62,27 +62,26 @@ class Report():
 
         Recursive implementation to select all the tags that form the category.
         If any tag is itself a cat, recursively iterates until tags found.
-        Implements memoization by saving every calculated category.
+        Implements memoization by saving sum of every calculated category.
         
         I.e. select "expenses". First tag is "car", so calls itself with cat=car
         and returns sum of car, then goes to "finance" and repeats.
 
         Args:
             cat (str): category to select.
+
+        Returns:
+            df (pd.DataFrame): categories in columns per time rows.
         """
         df = pd.DataFrame()
-        
-        for tag in self.cats[cat]:
-            if self.is_cat(tag):
+        for name in self.cats[cat]:
+            if self.is_cat(name):
                 try:
-                    srs = self.cdb[tag]
+                    df[name] = self.cdb[name]
                 except KeyError:
-                    srs = self.select_cat(tag).sum(axis=1)
-
-            elif tag in self.tdb.columns:
-                srs = self.tdb[tag]
-
-            df[tag] = srs
+                    df[name] = self.select_cat(name).sum(axis=1)
+            elif name in self.tdb.columns:
+                df[name] = self.tdb[name]
 
         self.cdb[cat] = df.sum(axis=1)      # Memoization
         return df
@@ -91,6 +90,47 @@ class Report():
         """Check if string `name` is a category defined in file CATS."""
         return bool(self.cats.get(name, False))
 
+    def timeline(self, name, dates=None):
+        """Obtain timeline of a group between dates (inclusive).
+
+        Args:
+            name (str): category or tag name.
+            dates (tuple): (t0, t1) str:"yyyy-mm-dd" or datetime.datetime.
+
+        Returns:
+            srs (pd.Series): group timeline with report's frequency.
+        """
+        if self.is_cat(name):
+            srs = self.select_cat(name).sum(axis=1)
+        else:
+            srs = self.tdb[name]
+        srs.name = name     # TODO necessary??
+        
+        if dates is None:
+            return srs
+        return srs[dates[0]: dates[1]]
+
+    def breakdown(self, cat, dates=None):
+        """Break down category into tags or subcats between `dates`(inclusive).
+
+        Args:
+            cat (str): category name.
+            dates (tuple): (t0, t1) str:"yyyy-mm-dd" or datetime.datetime.
+
+        Returns:
+            srs (pd.Series): sum of transactions per tag in specidied period.
+        """
+        df = self.select_cat(cat)
+        df.dropna(axis=1, how="all", inplace=True)
+
+        if dates is not None:
+            df = df.loc[dates[0]:dates[1], :]
+
+        srs = df.sum()
+        srs.name = cat
+        return srs
+
+        
 
 if __name__ == "__main__":
     # If module directly run, load log configuration for all modules.
